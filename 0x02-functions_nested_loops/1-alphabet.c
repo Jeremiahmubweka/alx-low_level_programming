b77c0c75b7be3d6860c121408b24c3a67d555366
jeremiahmubweka <jeremyorodi@gmail.com>
2022-12-15 04:15:26 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,15 +4,14 @@
  * print_alphabet - Print alphabet in lowercase using only `_putchar`
  * Return: 0
  */
-
 void print_alphabet(void)
 {
-	  	 char c = 'a';
+	char c = 'a';
 
-	   	while (c <= 'z')
-	   	{
-			   _putchar(c);
-			   c++;
-	   	}	
-	 		  _putchar('\n');
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
 }
